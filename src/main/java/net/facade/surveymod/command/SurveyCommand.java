779047625d6b94c7e52478b4d844d300c17f1968
Facade-0 <joshuarichardson4125@gmail.com>
2023-02-19 22:19:52 -0700

    Removed the MovePacket functionality. It is still called by "survey set foo bar".

--- src/main/java/net/facade/surveymod/command/SurveyCommand.java
+++ src/main/java/net/facade/surveymod/command/SurveyCommand.java
@@ -11,6 +11,9 @@ import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.LiteralTextContent;
+import net.minecraft.text.Text;
+import net.minecraft.text.TextContent;
 
 public class SurveyCommand {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess commandRegistryAccess, CommandManager.RegistrationEnvironment registrationEnvironment) {
@@ -28,16 +31,31 @@ public class SurveyCommand {
     }
 
     private static void movePlayer(ServerPlayerEntity player) {
-        PacketByteBuf buf = bufWritter(new int[]{0, 0, 10, 0});
+        PacketByteBuf buf = bufWritter(new int[]{0, 0, 1000, 0});
+        ServerPlayNetworking.send(player, ModMessages.MOVE_S2C, buf);
 
-        for(int i = 0; i < 600; i++) {
+        /*for(int i = 0; i < 1000; i++) {
             ServerPlayNetworking.send(player, ModMessages.MOVE_S2C, buf);
             try {
                 Thread.sleep(20);
             } catch (Exception e) {
                 // Lol
             }
+        }*/
+
+        Thread movementThread = new Thread();
+
+        for(int i = 0; i < 1; i++) {
+            buf = bufWritter(new int[]{i, 0, 0, 0});
+            ServerPlayNetworking.send(player, ModMessages.MOVE_S2C, buf);
+            try {
+                movementThread.sleep(50);
+            } catch (Exception e) {
+                // Lol
+            }
         }
+        player.sendMessageToClient(Text.of("DONE!"), false);
+
     }
 
     private static PacketByteBuf bufWritter(int[] inputs) {
--- src/main/java/net/facade/surveymod/networking/packet/MovePacket.java
+++ src/main/java/net/facade/surveymod/networking/packet/MovePacket.java
@@ -5,7 +5,13 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.PacketByteBuf;
+import net.minecraft.text.Text;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.input.Input;
+import net.minecraft.client.util.InputUtil;
+
+import java.util.function.BooleanSupplier;
 
 public class MovePacket {
     public static void receive(MinecraftClient client, ClientPlayNetworkHandler handler,
@@ -13,13 +19,45 @@ public class MovePacket {
         int[] inputs = buf.readIntArray();
         Vec3d movementVector = new Vec3d(inputs[0], inputs[1], inputs[2]);
         PlayerEntity player = client.player;
-        player.applyMovementInput(movementVector, inputs[3]);
 
-//        try {
-//            Thread.sleep(100);
-//        } catch (Exception e) {
-//            // lol
-//        }
+
+        //player.applyMovementInput(movementVector, inputs[3]);
+
+        //client.handleProfilerKeyPress(inputs[0]);
+
+        player.sendMessage(Text.of("The MovePacket is not longer in use!"));//, false);
+
+        /*Untested
+        player.addVelocity();
+        player.travel(movementVector);
+        client.getServer().getThread();
+        client.getServer().getTicks();
+        client.getServer().getTickTime();
+        client.getServer().getTaskCount();
+        client.getServer().tickWorlds();
+        */
+
+
+        /*Tested
+            client.tick();
+            client.getServer().tick(() -> true);
+            try {
+                Thread.sleep(20);
+            } catch (Exception e) {
+                // Lol
+            }
+        */
+
+        /*Thread movementThread = new Thread();
+
+        for(int i = 0; i < 1000; i++) {
+            player.applyMovementInput(movementVector, inputs[3]);
+            try {
+                movementThread.sleep(20);
+            } catch (Exception e) {
+                // Lol
+            }
+        }*/
     }
 
 }
